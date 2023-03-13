136cec7e3eace73fbbed73aad6f7d3102f955358
estebansant <estebansant30@gmail.com>
2023-03-13 16:46:43 -0300

    adding more pictures when clicking a button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,37 @@
+import {useState} from 'react';
+import type { MouseEventHandler } from 'react';
 import type {NextPage} from "next";
 import Head from 'next/head';
 import { RandomFox } from "../components/RandomFox";
 
+const random = () => Math.floor(Math.random() *123) +1;
+
+type ImageItem = { id: string; url: string}
+
+// generate random id
+const generateId = (): string => {
+  return (
+    Math.random().toString(36).substring(2, 15) +
+    Math.random().toString(36).substring(2, 15)
+  );
+};
+
 export default function Home() {
+
+  const [images, setImages] = useState<Array<ImageItem>>([]);
+
+  const addNewFox: MouseEventHandler<HTMLButtonElement> = (event) => {
+    const newImageItem: ImageItem = {
+      id: generateId(),
+      url: `https://randomfox.ca/images/${random()}.jpg`
+    };
+
+    setImages([
+      ...images,
+      newImageItem
+    ])
+  }
+
   return (
     <>
       <Head>
@@ -13,7 +42,15 @@ export default function Home() {
       </Head>
       <main>
         <h1 class="text-3xl font-bold underline">Hello, world!</h1>
-        <RandomFox />
+        <button onClick={addNewFox}>
+          Add new Fox
+        </button>
+        {images.map(({id, url}) => (
+          <div key={id}className="p-4">
+            <RandomFox image={url} />
+          </div>
+        ))}
+        
       </main>
 
       <footer>
